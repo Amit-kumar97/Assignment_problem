2ea50e33ad617ce426a07ad99c8b9ec3863204ea
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-03-28 22:26:22 +0530

    Update QueenMoves.java

--- QueenMoves.java
+++ QueenMoves.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class QueenMoves {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bishop Moves in 8*8 Chess Board.");
+        System.out.println("Bishop Moves in n*n Chess Board.");
+        System.out.println("Enter the Size of Chess Board:");
+        int size = sc.nextInt();
         System.out.println("Enter the Number of Rows And Columns:");
         int row = sc.nextInt();
         int col = sc.nextInt();
         System.out.println("Total number of Possible Moves:");
-        System.out.println(queencount(row,col));
+        System.out.println(queencount(row,col,size));
 
     }
-    public static int queencount(int row, int col)
+    public static int queencount(int row, int col,int size)
     {
-        if (row<=8 && col<=8)
+        if (row<=size && col<=size)
         {
-            return (Math.min(row-1,col-1)+Math.min(row-1,8-col)+Math.min(8-row,col-1)+Math.min(8-row,8-col)+(8-1)+(8-1));
+            return (Math.min(row-1,col-1)+Math.min(row-1,size-col)+Math.min(size-row,col-1)+Math.min(size-row,size-col)+(size-1)+(size-1));
         }
         else
         {
@@ -23,3 +25,4 @@ public class QueenMoves {
         }
     }
 }
+
