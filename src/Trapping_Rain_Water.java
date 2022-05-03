29151f077bcd5642fa2ae43ddd9e04c0670aa30d
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-05-03 22:30:52 +0530

    Trapping rainwater problem

---
+++ src/Trapping_Rain_Water.java
@@ -0,0 +1,84 @@
+
+/*
+Trapping rainwater problem:
+Find the maximum amount of water that can be trapped within a given
+set of bars where each bar’s width is 1 unit.
+
+For example:
+Input: An array containing height of bars {7, 0, 4, 2, 5, 0, 6, 4, 0, 5}
+
+|---| 6   2   4   1   6
+|   |~~~~~~~~~~~~~~~~~~~|---| 1   5
+|   |           |---|   |   |~~~~~~~|---|
+|   |   |---|   |   |   |   |---|   |   |
+|   |   |   |   |   |   |   |   |   |   |
+|   |   |   |---|   |   |   |   |   |   |
+|___|___|___|___|___|___|___|___|___|___|
+  7   0   4   2   5   0   6   4   0   5
+
+The maximum amount of water that can be trapped is 25.
+
+ */
+
+public class Trapping_Rain_Water {
+
+    public static void main(String[] args) {
+        int[] heights = { 7, 0, 4, 2, 5, 0, 6, 4, 0, 5 };
+
+        System.out.print("The maximum amount of water that can be trapped is =" + trap(heights));
+    }
+
+    // Function to find the amount of water that can be trapped within
+    // a given set of bars in linear time and extra space
+    public static int trap(int[] bars)
+    {
+        int n = bars.length;
+
+        // base case
+        if (n <= 2) {
+            return 0;
+        }
+
+        int water = 0;
+
+        // `left[i]` stores the maximum height of a bar to the left of the current bar
+        int[] left = new int[n-1];
+        left[0] = Integer.MIN_VALUE;
+
+        // process bars from left to right
+        for (int i = 1; i < n - 1; i++) {
+            left[i] = Integer.max(left[i - 1], bars[i - 1]);
+        }
+
+        /*
+        int[] right = new int[n];
+        right[n - 1] = Integer.MIN_VALUE;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Integer.max(right[i + 1], bars[i + 1]);
+        }
+
+        for (int i = 1; i < n - 1; i++)
+        {
+            if (Integer.min(left[i], right[i]) > bars[i]) {
+                water += Integer.min(left[i], right[i]) - bars[i];
+            }
+        }
+        */
+
+        // `right` stores the maximum height of a bar to the right of the current bar
+        int right = Integer.MIN_VALUE;
+
+        // process bars from right to left
+        for (int i = n - 2; i >= 1; i--)
+        {
+            right = Integer.max(right, bars[i + 1]);
+
+            // check if it is possible to store water in the current bar
+            if (Integer.min(left[i], right) > bars[i]) {
+                water += Integer.min(left[i], right) - bars[i];
+            }
+        }
+
+        return water;
+    }
+}
